d69a7fdf116a656587a2b42ff92cee68220c75ea
xtej4ik <chtejinna@gmail.com>
2023-04-21 21:42:13 +0100

    user card css

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -8,10 +8,44 @@ export const UserCard = ({ user, setUser }) => {
         navigate('/')
     }
     return (
+        
         <div>
+            <br>
+            </br>
+            <br>
+            </br>
+
+
+            <div class="flip-card">
+            <div class="flip-card-inner">
+            <div class="flip-card-front">
+            <img src={user.avatar_url} alt="avatar" ></img>
+        
+            </div>
+            <div class="flip-card-back">
+            <p>{user.name}</p> 
             
+             </div>
+            </div>
+            </div>
+            <br>
+            </br>
+            <br>
+            </br>
+
+            <button classnName="userbutton" onClick={impersonate}>login as</button>
+            <br>
+            </br>
+            <br>
+            </br>
+            <br>
+            </br>
+            <br>
+            </br>
+            <br>
+            </br>
+           
             
-            <button onClick={impersonate}>login as</button>
         </div>
     )
 }
