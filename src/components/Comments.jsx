d99e855247fc7a49960c431d314ec1bd85055bc6
xtej4ik <chtejinna@gmail.com>
2023-05-26 11:05:57 +0100

    first on a list after posting comment logic added

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -3,92 +3,95 @@ import { fetchComments, commentAdder } from "../api"
 import "../styles.css"
 import { UserContext } from "../contexts/User";
 
-
 function Comments({ article_id }) {
-    const [isLoading, setLoading] = useState(true);
-    const [comments, setComments] = useState([]);
-    const [isExpanded, setExpanded] = useState(false);
+  const [isLoading, setLoading] = useState(true);
+  const [comments, setComments] = useState([]);
+  const [isExpanded, setExpanded] = useState(false);
 
-    const [commentSubmitted, setCommentSubmitted] = useState(false);
-    const [isSubmitting, setIsSubmitting] = useState(false);
-    const [submitError, setSubmitError] = useState(null);
-    const [newComment, setNewComment] = useState('');
-    const user = useContext(UserContext)
-    
-    useEffect(() => {
-        fetchComments(article_id).then((commentsData) => {
-            setComments(commentsData);
-            setLoading(false);
-        });
+  const [commentSubmitted, setCommentSubmitted] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [submitError, setSubmitError] = useState(null);
+  const [newComment, setNewComment] = useState('');
+  const user = useContext(UserContext)
 
-    }, [article_id]);
+  useEffect(() => {
+    fetchComments(article_id).then((commentsData) => {
+      setComments(commentsData);
+      setLoading(false);
+    });
+  }, [article_id]);
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        commentAdder(article_id, { body: newComment, username: user.username })
-            .then((response) => {
-                setComments([...comments, response]);
-                setCommentSubmitted(true);
-                setNewComment("");
-            })
-            .catch((error) => {
-                setSubmitError(error.message);
-            })
-            .finally(() => {
-                setIsSubmitting(false);
-            });
-            setIsSubmitting(true);
-            setSubmitError(null);
-        };
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
+    if (newComment.trim() === '') {
+      setSubmitError("Oops! You forgot to write a comment.");
+      return;
+    }
 
-    const handleToggleClick = () => {
-        setExpanded(!isExpanded);
-    };
+    setIsSubmitting(true);
+    setSubmitError(null);
 
-    return (
-        <section id="comments">
-        {user ? (
-            <div>
-            <form onSubmit={handleSubmit}>
+    commentAdder(article_id, { body: newComment, username: user.username })
+      .then((response) => {
+        setComments((prevComments) => [...prevComments, response]);
+        setCommentSubmitted(true);
+        setNewComment("");
+      })
+      .catch((error) => {
+        setSubmitError(error.message);
+      })
+      .finally(() => {
+        setIsSubmitting(false);
+      });
+  };
+
+  const handleToggleClick = () => {
+    setExpanded(!isExpanded);
+  };
+
+  return (
+    <section id="comments">
+      {user ? (
+        <div>
+          <form onSubmit={handleSubmit}>
             <label>
-                Add a new comment:
-                <textarea
-                    value={newComment}
-                    onChange={(event) => setNewComment(event.target.value)}
-                />
+              Add a new comment:
+              <textarea
+                value={newComment}
+                onChange={(event) => setNewComment(event.target.value)}
+              />
             </label>
             {submitError && <p>Error: {submitError}</p>}
             <button type="submit">Submit</button>
-              {commentSubmitted && <p>Comment submitted!</p>}
-              </form> 
-              </div>
+            {commentSubmitted && <p>Comment submitted!</p>}
+          </form>
+        </div>
+      ) : (
+        <p>Sorry, only registered users can comment. Please log in to participate</p>
+      )}
+      <button className="accordion" onClick={handleToggleClick}>
+        {isExpanded ? "Hide comments" : "Show comments"}
+      </button>
+      <div className={`panel ${isExpanded ? "expanded" : ""}`}>
+        {isLoading ? (
+          <h3>Loading...</h3>
+        ) : comments.length === 0 ? (
+          <p>No comments</p>
         ) : (
-            <p>Sorry, only registered users can comment. Please log in to participate</p>
+          <ul>
+           {comments.slice().reverse().map((comment) => (
+              <li key={comment.comment_id}>
+                <p>{comment.body}</p>
+                <p>Author: {comment.author}</p>
+                <time>Created at: {comment.created_at}</time>
+              </li>
+            ))}
+          </ul>
         )}
-                <button className="accordion" onClick={handleToggleClick}>
-                    {isExpanded ? "Hide comments" : "Show comments"}
-                </button>
-                <div className={`panel ${isExpanded ? "expanded" : ""}`}>
-                    {isLoading ? (
-                        <h3>Loading...</h3>
-                    ) : comments.length === 0 ? (
-                        <p>No comments</p>
-                    ) : (
-                        <ul>
-                            {comments.map((comment) => (
-                                <li key={comment.comment_id}>
-                                    <p>{comment.body}</p>
-                                    <p>Author: {comment.author}</p>
-                                    <time>Created at: {comment.created_at}</time>
-                                </li>
-                            ))}
-                        </ul>
-                    )}
-                </div>
-            </section>
-                        
-    );
+      </div>
+    </section>
+  );
 }
 
 export default Comments;
